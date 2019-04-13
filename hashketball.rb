ae41bb9535162c93f38ad89151f8337c5f46c41d
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-13 21:40:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -199,6 +199,7 @@ def player_stats(player_name)
     team_info.each do |info_types, info|
       if info.include?(player_name)
       if info_types == :players
+        binding.pry
         stats= info.fetch_values(player_name)
       end
       return stats
