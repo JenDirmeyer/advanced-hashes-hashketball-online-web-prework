7d5bd7658de67540306a0b89a5cc326c95bff405
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-13 22:28:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -226,7 +226,7 @@ def big_shoe_rebounds
     team_info.each do |info_types, info|
       if info_types == :players
         info.each do |players, categories|
-          players.each do |cats, stats|
+          #players.each do |cats, stats|
             players.key(largest)
             binding.pry
           end  
